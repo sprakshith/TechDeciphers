8cf8e5dbc45cbb64b7de8ce16a91cab1e0a53301
SP Rakshith <rakshith0908@gmail.com>
2020-06-06 23:02:26 +0530

    Just Trying Again

--- TechDeciphers/TechDeciphers_Home/views.py
+++ TechDeciphers/TechDeciphers_Home/views.py
@@ -2,5 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Inside TechDeciphers Home Page, Templates are yet to be Created. Copy and Paste any of the below Links. ' +
+    return HttpResponse('Inside the TechDeciphers Home Page, Templates are yet to be Created. Copy and Paste any of the below Links. ' +
                          'Append any one of these links to the URL : /gadgets , /news , /topX , /leaks')
