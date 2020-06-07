204a5f2127aec7631536411d7595f73947f0e181
SP Rakshith <rakshith0908@gmail.com>
2020-06-07 14:40:59 +0530

    Setting Allowed Host

--- TechDeciphers/TechDeciphers/settings.py
+++ TechDeciphers/TechDeciphers/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'd68lujn#97^#eg727w_bvdj((+(7x8@wvhhv5qt_z6)f6s*wo@'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sprakshith.pythonanywhere.com']
 
 
 # Application definition
