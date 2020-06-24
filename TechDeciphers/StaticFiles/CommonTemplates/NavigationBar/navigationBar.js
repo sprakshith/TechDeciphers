3fe9fbaa4cc02633aff969d727bdc234680a9009
SP Rakshith <rakshith0908@gmail.com>
2020-06-24 21:30:05 +0530

    Hamburger

--- TechDeciphers/StaticFiles/CommonTemplates/NavigationBar/navigationBar.js
+++ TechDeciphers/StaticFiles/CommonTemplates/NavigationBar/navigationBar.js
@@ -2,8 +2,12 @@ function myFunction() {
   var navigationLinks = document.getElementById("myLinks");
   if (navigationLinks.style.display === "block") {
     $("#myLinks").css({'display' : 'none'});
+    $(".topnav").css({'height' : '40px'});
+    console.log("Hide Navigation Bar");
   }
   else {
     $("#myLinks").css({'display' : 'block'});
+    $(".topnav").css({'height' : '235px'});
+    console.log("Show Navigation Bar");
   }
 }
