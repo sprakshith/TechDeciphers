39bea328e4b22f6b2bd19c8e29024e139abce5ff
SP Rakshith <rakshith0908@gmail.com>
2020-06-28 17:16:04 +0530

    Minor Changes in Gadgets Page

--- TechDeciphers/StaticFiles/CommonTemplates/NavigationBar/navigationBar.js
+++ TechDeciphers/StaticFiles/CommonTemplates/NavigationBar/navigationBar.js
@@ -1,13 +1,13 @@
 function myFunction() {
   var navigationLinks = document.getElementById("myLinks");
   if (navigationLinks.style.display === "block") {
+    $('#techDeciphersHeading').css({'display' : 'block'});
     $("#myLinks").css({'display' : 'none'});
     $(".topnav").css({'height' : '40px'});
-    console.log("Hide Navigation Bar");
   }
   else {
+    $('#techDeciphersHeading').css({'display' : 'none'});
     $("#myLinks").css({'display' : 'block'});
-    $(".topnav").css({'height' : '235px'});
-    console.log("Show Navigation Bar");
+    $(".topnav").css({'height' : '270px'});
   }
 }
