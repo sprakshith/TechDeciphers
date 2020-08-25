d65a6fc1c2ac27a97287d6aecf3383030fb47f50
SP Rakshith <rakshith0908@gmail.com>
2020-08-25 21:52:55 +0530

    ChatBot First Command

--- TechDeciphers/TechDeciphers_ChatBotService/views.py
+++ TechDeciphers/TechDeciphers_ChatBotService/views.py
@@ -7,16 +7,15 @@ from TechDeciphers_News.models import News
 from TechDeciphers_Leaks.models import Leaks
 from TechDeciphers_TopX.models import TopX
 
-from TechDeciphers_ChatBotService.ChatBotUtil.topNewGadgets import getTopNewsGadget
-
 @csrf_exempt
 def index(request):
     receivedString = request.POST.get('Body', 'NA')
     r = MessagingResponse()
-    print(getTopNewsGadget(""))
 
-    if receivedString == '/showTodaysNews':
-        r.message(getTopNewsGadget(""))
+    if receivedString == '/help':
+        r.message("This is UnderConstuction, we will be back Soon")
+    elif receivedString == '/showUnPulishedArticle':
+        r.message("Unpublished Articles Will be Show Here")
     else:
         r.message("Sorry, your Command is Not Recognised")
 
