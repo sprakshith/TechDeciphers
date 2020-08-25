ebea4681bd216f9f98c13377baa6e62303031fbc
SP Rakshith <rakshith0908@gmail.com>
2020-08-25 22:08:07 +0530

    Commands Added

--- TechDeciphers/TechDeciphers_ChatBotService/views.py
+++ TechDeciphers/TechDeciphers_ChatBotService/views.py
@@ -7,16 +7,27 @@ from TechDeciphers_News.models import News
 from TechDeciphers_Leaks.models import Leaks
 from TechDeciphers_TopX.models import TopX
 
+def getCurrentCommands():
+    commands = '''
+                  Below are the Commands, you can use Numbers or Type the Command Itself\n
+                  1. ShowUnpublishedArticle - This Shows all the Unpublished Articles\n
+                  2. WeeksArticleTarget - This Shows the remaining amounts of Articles to be uploaded this week
+               '''
+
+    return commands
+
 @csrf_exempt
 def index(request):
     receivedString = request.POST.get('Body', 'NA')
     r = MessagingResponse()
 
-    if receivedString == '/help':
-        r.message("This is UnderConstuction, we will be back Soon")
-    elif receivedString == '/showUnPulishedArticle':
-        r.message("Unpublished Articles Will be Show Here")
+    if receivedString == 'Help' or receivedString == '0':
+        r.message(getCurrentCommands())
+    elif receivedString == 'ShowUnpublishedArticle' or receivedString == '1':
+        r.message("There are No Unpublished Articles right Now")
+    elif receivedString == 'WeeksArticleTarget' or receivedString == '2':
+        r.message("Gagan : 1 Remaining\n Rakshith : 2 Remaining")
     else:
-        r.message("Sorry, your Command is Not Recognised")
+        r.message("Sorry, your Command is Not Recognised. Send 'Help' to get all the commands. Thank You.")
 
     return HttpResponse(r, content_type='text/xml')
