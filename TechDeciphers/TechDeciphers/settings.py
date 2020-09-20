941a3e5c991fa68f227251086ebf0073ec70bf85
SP Rakshith <rakshith0908@gmail.com>
2020-09-20 18:41:54 +0530

    Adding MYSQL Code

--- TechDeciphers/TechDeciphers/settings.py
+++ TechDeciphers/TechDeciphers/settings.py
@@ -85,8 +85,12 @@ WSGI_APPLICATION = 'TechDeciphers.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'sprakshith$techdeciphers',
+        'USER': 'sprakshith',
+        'PASSWORD': 'techroot',
+        'HOST': 'sprakshith.mysql.pythonanywhere-services.com',
+        'PORT': '3306'
     }
 }
 
